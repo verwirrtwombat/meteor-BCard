683418f7ffce0c04a17ac2d7428aa562f3d9a9fc
Kevin Nelson <kpie314@gmail.com>
2016-03-22 08:03:02 -0700

    add preview

--- client/newForm/newForm.js
+++ client/newForm/newForm.js
@@ -1,5 +1,24 @@
 Template.newForm.events({
-	'submit form': function(event, template) {
+	'submit .f1': function(event, template) {
+		
+		event.preventDefault();
+
+		let firstName = template.find('#firstName').value;
+		let lastName = template.find('#lastName').value;
+		let quote = template.find('#quote').value;
+		let gender = template.find('#gender').value;
+		let githubLink = template.find('#githubLink').value;
+		Session.set('firstName',firstName);
+		Session.set('quote',quote);
+		Session.set('lastname',lastName);
+		Session.set('gender',gender);
+		Session.set('githubLink',githubLink);
+		
+		//Router.go(`/${firstName}`);
+		Router.go("/new");
+		
+	},
+	'submit .f2': function(event, template) {
 		
 		event.preventDefault();
 
@@ -15,6 +34,7 @@ Template.newForm.events({
 		Session.set('githubLink',githubLink);
 		
 		Router.go(`/${firstName}`);
+		//Router.go("/new");
 		
 	}
 });
